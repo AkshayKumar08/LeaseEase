e5b185e976b418ee960e2c5769ae94cf876fad0c
Akshay Kumar <rmakshaykumar08@gmail.com>
2023-12-28 17:20:37 -0600

    initial commit

---
+++ src/test/java/com/aksrmk/LeaseEase/LeaseEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksrmk.LeaseEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaseEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
